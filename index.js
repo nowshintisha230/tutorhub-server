5fdd494d58ca5cfe01d7f281adec7dee96e4899e
Rieadh Sayfullah <you@example.com>
2026-05-18 00:41:36 +0600

    Fetch data from database and display it in UI

--- index.js
+++ index.js
@@ -5,11 +5,14 @@ dns.setServers(["8.8.8.8", "8.8.4.4"]);
 
 const express = require('express')
 const dotenv=require('dotenv')
+const cors=require("cors");
 const { MongoClient, ServerApiVersion } = require('mongodb');
 dotenv.config()
 const uri = process.env.MONGODB_URI;
 const app = express()
 const port = process.env.PORT
+app.use(cors())
+app.use(express.json())
 
 const client = new MongoClient(uri, {
   serverApi: {
@@ -21,14 +24,32 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
+
     await client.connect();
-    // Send a ping to confirm a successful connection
+   
+const db =client.db("tutorhub")
+const tutorCollection =db.collection("tutor")
+
+app.get('/tutor',async(req,res)=>{
+    const result=await tutorCollection.find().toArray()
+    res.json(result);
+})
+
+
+
+app.post('/tutor',async(req,res)=>{
+    const tutorData= req.body
+    console.log(tutorData)
+ const result = await tutorCollection.insertOne(tutorData)
+ res.json(result)
+})
+
+
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
